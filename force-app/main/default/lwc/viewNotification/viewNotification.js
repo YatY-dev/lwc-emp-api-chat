c89a418cc4a9e52a75c7313b3d0467ff6b40ed6d
YatY-dev <33408240+YatY-dev@users.noreply.github.com>
2023-11-01 23:56:39 +0900

    JSファイルの追記

--- force-app/main/default/lwc/viewNotification/viewNotification.js
+++ force-app/main/default/lwc/viewNotification/viewNotification.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire } from "lwc";
 import { subscribe, unsubscribe, isEmpEnabled } from "lightning/empApi";
 import getUserData from "@salesforce/apex/ViewNotificationController.getUserData";
+import getUserDataById from "@salesforce/apex/ViewNotificationController.getUserDataById";
 
 export default class ViewNotification extends LightningElement {
   @api objectApiName;
@@ -35,18 +36,45 @@ export default class ViewNotification extends LightningElement {
       this.error = "unavailable";
     }
 
-    getUserData().then(data => {
-      this.userId = this.generateChatUserId(data.Id);
-      this.userName = data.Name;
-      this.profileType = "url";
-      this.profileValue = data.SmallPhotoUrl;
-      this.profileImageUrl = data.SmallPhotoUrl;
-      this.error = null;
-    })
-    .catch(error => {
-      this.error = error;
-    });
+    getUserData()
+      .then((data) => {
+        this.userId = this.generateChatUserId(data.Id);
+        this.userName = data.Name;
+        this.profileType = "url";
+        this.profileValue = data.SmallPhotoUrl;
+        this.profileImageUrl = data.SmallPhotoUrl;
+        this.error = null;
+      })
+      .catch((error) => {
+        this.error = error;
+      });
+    
+    const subscription = await subscribe(
+      this.channelName,
+      -1,
+      (event) => this.handleNotificationEvent(event)
+    );
+    this.subscription = subscription;
 
+  };
+
+  // Callback invoked whenever a new event message is received
+  async handleNotificationEvent(event) {
+    console.dir(event);
+    const objectName = event.data.payload.ObjectName__c;
+    const receiveRecordId = event.data.payload.RecordId__c;
+    const viewDateTime = new Date(event.data.payload.ViewDateTime__c);
+    const viewUserId = event.data.payload.viewUserId__c;
+
+    if (objectApiName == objectName && this.recordId == receiveRecordId) return;
+
+    const user = await getUserDataById(viewUserId);
+    this.dispatchEvent(
+      new ShowToastEvent({
+        variant: "info",
+        title: message
+      })
+    );
   };
 
   generateChatUserId(userId) {
