efe8c5c1a60028c7d3cd05ea6706c3a926ea2aeb
YatY-dev <33408240+YatY-dev@users.noreply.github.com>
2023-11-01 23:27:11 +0900

    カスタム処理の追加

---
+++ force-app/main/default/lwc/viewNotification/viewNotification.js
@@ -0,0 +1,50 @@
+import { LightningElement, api, wire } from "lwc";
+import { subscribe, unsubscribe, isEmpEnabled } from "lightning/empApi";
+import getUserData from "@salesforce/apex/ViewNotificationController.getUserData";
+
+export default class ViewNotification extends LightningElement {
+  @api objectApiName;
+  @api recordId;
+  channelName = "/event/ViewNotification__e";
+
+  subscription = {};
+
+  userId = "";
+  userName = "";
+  profileType = "";
+  profileValue = "";
+  profileImageUrl;
+
+  @wire(getUserData)
+  wiredUserData({ error, data }) {
+    if (data) {
+      this.userId = this.generateChatUserId(data.Id);
+      this.userName = data.Name;
+      this.profileType = "url";
+      this.profileValue = data.SmallPhotoUrl;
+      this.profileImageUrl = data.SmallPhotoUrl;
+      this.error = null;
+    } else if (error) {
+      this.error = error;
+    }
+  }
+
+  connectedCallback = async () => {
+    const isEmpAvailable = await isEmpEnabled();
+    if (!isEmpAvailable) {
+      this.error = "unavailable";
+    }
+1
+    getUserData().then(data => {
+    
+    })
+    .catch(error => {
+
+    });
+
+  };
+
+  generateChatUserId(userId) {
+    return userId + Math.floor(Math.random() * 10000) + Date.now();
+  }
+}
